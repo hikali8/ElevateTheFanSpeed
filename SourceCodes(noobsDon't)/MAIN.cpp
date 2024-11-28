ccb52963d30e180998b14100fe6f540ac00606e4
hikali8 <90106349+hikali8@users.noreply.github.com>
2024-11-28 11:56:51 +0800

    Create MAIN.cpp

---
+++ SourceCodes(noobsDon't)/MAIN.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <windows.h>
+
+#include "ec.hpp"
+
+BOOL IsRunAsAdministrator()
+{
+    BOOL fIsRunAsAdmin = FALSE;
+    DWORD dwError = ERROR_SUCCESS;
+    PSID pAdministratorsGroup = NULL;
+
+    SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+    if (!AllocateAndInitializeSid(
+        &NtAuthority,
+        2,
+        SECURITY_BUILTIN_DOMAIN_RID,
+        DOMAIN_ALIAS_RID_ADMINS,
+        0, 0, 0, 0, 0, 0,
+        &pAdministratorsGroup))
+    {
+        dwError = GetLastError();
+        goto Cleanup;
+    }
+
+    if (!CheckTokenMembership(NULL, pAdministratorsGroup, &fIsRunAsAdmin))
+    {
+        dwError = GetLastError();
+        goto Cleanup;
+    }
+
+Cleanup:
+
+    if (pAdministratorsGroup)
+    {
+        FreeSid(pAdministratorsGroup);
+        pAdministratorsGroup = NULL;
+    }
+
+    if (ERROR_SUCCESS != dwError)
+    {
+        throw dwError;
+    }
+
+    return fIsRunAsAdmin;
+}
+
+void ElevateNow()
+{
+    BOOL bAlreadyRunningAsAdministrator = FALSE;
+    try
+    {
+        bAlreadyRunningAsAdministrator = IsRunAsAdministrator();
+    }
+	catch(...){}
+    if (!bAlreadyRunningAsAdministrator)
+    {
+        TCHAR szPath[MAX_PATH];
+        if (GetModuleFileName(NULL, szPath, ARRAYSIZE(szPath)))
+        {
+            SHELLEXECUTEINFO sei = { sizeof(sei) };
+
+            sei.lpVerb = "runas";
+            sei.lpFile = szPath;
+            sei.hwnd = NULL;
+            sei.nShow = SW_SHOWDEFAULT;
+
+            if (!ShellExecuteEx(&sei))
+            {
+                DWORD dwError = GetLastError();
+                if (dwError == ERROR_CANCELLED)
+                    //Annoys you to Elevate it LOL
+                    CreateThread(0, 0, (LPTHREAD_START_ROUTINE)ElevateNow, 0, 0, 0);
+            }
+        }
+
+    }
+}
+
+
+
+int main()
+{
+	ElevateNow();
+	
+	EmbeddedController ec = EmbeddedController();
+	for(int i = 3; i--;){
+		if(ec.readByte(189) == 0x80)
+			return 0;
+		if (ec.driverFileExist && ec.driverLoaded)
+			ec.writeByte(189, 0x80);
+		Sleep(1);
+	}
+	std::cout << "Failed!" << std::endl;
+	return 1;
+}
